f2c7db1a0c244649c0ddbfee868c41fca5378876
bonkzwonil <bonkzwonil@gmail.com>
2025-09-01 10:22:38 +0200

    small changes

--- makemovie.sh
+++ makemovie.sh
@@ -2,9 +2,11 @@
 
 # Movie example, you need quicklisp and mencoder for this to work
 STARTP=$1
+ITER=$2
 
 echo rendering movie....
 sbcl --non-interactive --load mandel.fasl --load threading.fasl --load gfx.lisp \
+		 --eval "(setf *iterations* ${ITER:=400})" \
 		 --eval "(init-travel '${STARTP:=sun})" --eval "(setf *VIEWPORT* (next-vp))" --eval  "(MAKE-MOVIE-MP)"
 
 mencoder mf://image*.png -mf fps=25 -ovc x264 -oac copy -o output.avi
