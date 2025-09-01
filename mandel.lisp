e94e52d77b29148b260744925e846ad16a69b648
bonkzwonil <bonkzwonil@gmail.com>
2025-09-01 10:22:38 +0200

    refactored

--- mandel.lisp
+++ mandel.lisp
@@ -3,7 +3,7 @@
 (defparameter *viewport*
 	'((-2.0d0 . 1.2d0)  (1d0 . -1.2d0)))
 
-(defparameter threshold 3.0d0)
+(defparameter *threshold* (the fixnum 3))
 
 (defparameter *iterations* 100)
 
@@ -11,31 +11,44 @@
 
 (defparameter *interactive-p* t) ;; Interactive Mode: move with w,a,s,d , accelerate/decelerte with k,l
 
+;;SOme interesting coords:
+
+(defparameter *mandel-travel-map*
+	'((spirals -0.343806077d0 -0.611278040)
+		(starfish -0.374004139 -0.659792175)
+		(trunks 0.001643721971153d0 0.82246763329887d0)
+		(julia -1.768778833d0 0.001738996d0)
+		(juliazoom -1.768778832d0 0.001738917)
+		(flower -1.999985882d0 0d0)
+		(home 0d0 0d0)
+		(seahorse -0.743517833d0 0.127094578d0)
+		(sun -0.776592024d0 0.136640779d0)
+		(tendrils -0.226266648d0 -1.11617444d0)))
+
+
+;;Unoptimized & naive
 (defun mandel (c z)
-	(declare (optimize (speed 3) (safety 0)))
-  (declare (type complex c))
-  (declare (type complex z))
 	(the complex (+ (* z z)
 									c)))
 
 (defun mandel-iter (c &optional (n *iterations*) (z #c(0s0 0s0)))
-	"iterates c n-times with mandel"
-	(declare (optimize (speed 3) (safety 0)))
-  (declare (type fixnum n))
-  (declare (type complex c))
-  (declare (type complex z))
-  (declare (type fixnum threshold))
+	"iterates c n-times with mandel, unoptimized and slow"
 	(let ((i 0))
 		(loop while (and (< i n)
-										 (< (abs z) threshold))
+										 (< (abs z) *threshold*)) ;FIXME
 					do
 						 (setf z (mandel c z))
 						 (incf i))
 		z))
 
-(defun in-mandelset-p (c &optional (iterations *iterations*))
+(defun in-mandelset-p (c &key (iterations *iterations*) (threshold *threshold*))
 	(>= (abs (mandel-iter c iterations))
 			threshold))
+
+
+;; -----------
+;;  Viewport and axis mapping
+;;
 
 (defun viewport-size (&optional (viewport *viewport*))
 	(let* ((v1 (car viewport))
@@ -44,58 +57,8 @@
 				 (vh (- (cdr v2) (cdr v1))))
 		(cons vw vh)))
 	
-(defun coords->c (x y w h )
-	"Hilfsfkt: koords in viewport c umrechnen"
-	(let* ((vs (viewport-size))
-				 (xn (/ (car vs) w))
-				 (yn (/ (cdr vs) h)))
-	(complex (+ (* xn x) (caar *viewport*))
-					 (+ (* yn y) (cdar *viewport*)))))
 
 
-;; (defun z->ascii (z &optional (threshold threshold))
-;; 	"Asci<<farbe>> bauen"
-;; 	(let* ((mina #\A)
-;; 				 (maxa #\Z)
-;; 				 (w (- (char-code maxa) (char-code mina))))
-;; 		(code-char (round (+ (char-code mina) (* (/ z threshold) w))))))
-
-(defun z->ascii (z &optional (threshold threshold))
-	"Asci<<farbe>> bauen"
-	(if (>= z threshold)
-			" "
-			(let* ((mina #\.)
-						 (maxa #\z)
-						 (w (- (char-code maxa) (char-code mina))))
-				(if (> z 1)
-						maxa
-						(code-char (round (+ (char-code mina) (* z w) )))))))
-
-(defun z->ascii2 (i &optional (iterations *iterations*))
-	"Asci<<farbe>> bauen (2. methode: nach iter"
-	(if (>= i iterations)
-			" "
-			(let* ((mina #\.)
-						 (maxa #\z)
-						 (w (- (char-code maxa) (char-code mina))))
-				(if maxa
-						(code-char (round (+ (char-code mina) (* (/ i *iterations*) w))))))))
-
-
-(defun asciipaint (w h)
-	(setf *pxs* 0)
-	(loop for y from 0 to (1- h) do
-		(loop for x from 0 to (1- w) do
-			(multiple-value-bind (z i) (m4nde1-1t3r (coords->c x y w h))
-				(if (< i *iterations*) (incf *pxs*)) ;; just for exiting
-				(princ 	(z->ascii2 i))))
-		(princ #\Newline))
-	(home))
-
-
-
-
-	
 
 (defun calc-viewport (r i w)
 	"simple viewport set centered on r,i w height and width"
@@ -109,14 +72,6 @@
 (defun set-viewport (r i w)
 	"simple viewport set centered on r,i w height and width"
 	(setf *viewport* (calc-viewport r i w)))
-
-;(defparameter *r* -0.6179728241319444d0)
-;(defparameter *i* 0.4518895494791668d0)
-
-;(defparameter *r* 0.001643721971153d0)
-;(defparameter *i* 0.82246763329887d0)
-																				;-0.61886875
-																				;0.44718610937500014
 
 (defun init-coords (r i &optional (speed 0.95d0))
 	"sets coordinate system on center point and builds transform functions"
@@ -138,24 +93,61 @@
 			(setf di 0d0))))
 
 
-;;SOme interesting coords:
-
-(defparameter *mandel-travel-map*
-	'((spirals -0.343806077d0 -0.611278040)
-		(starfish -0.374004139 -0.659792175)
-		(trunks 0.001643721971153d0 0.82246763329887d0)
-		(julia -1.768778833d0 0.001738996d0)
-		(juliazoom -1.768778832d0 0.001738917)
-		(flower -1.999985882d0 0d0)
-		(home 0d0 0d0)
-		(seahorse -0.743517833d0 0.127094578d0)
-		(sun -0.776592024d0 0.136640779d0)
-		(tendrils -0.226266648d0 -1.11617444d0)))
+(defun coords->c (x y w h )
+	"Hilfsfkt: koords in viewport c umrechnen"
+	(let* ((vs (viewport-size))
+				 (xn (/ (car vs) w))
+				 (yn (/ (cdr vs) h)))
+	(complex (+ (* xn x) (caar *viewport*))
+					 (+ (* yn y) (cdar *viewport*)))))
 
 (defun init-travel (name &optional (speed 0.95d0))
 	(let ((coords (assoc name *mandel-travel-map*)))
 		(when coords
 			(init-coords (cadr coords) (caddr coords) speed))))
+
+
+;; ,,Coloring''
+
+(defun z->ascii (z &optional (threshold *threshold*))
+	"Asci<<farbe>> bauen"
+	(if (>= z threshold)
+			" "
+			(let* ((mina #\.)
+						 (maxa #\z)
+						 (w (- (char-code maxa) (char-code mina))))
+				(if (> z 1)
+						maxa
+						(code-char (round (+ (char-code mina) (* z w) )))))))
+
+(defun z->ascii2 (i &optional (iterations *iterations*))
+	"Asci<<farbe>> bauen (2. methode: nach iter"
+	(if (>= i iterations)
+			" "
+			(let* ((mina #\.)
+						 (maxa #\z)
+						 (w (- (char-code maxa) (char-code mina))))
+				(if maxa
+						(code-char (round (+ (char-code mina) (* (/ i *iterations*) w))))))))
+
+
+
+;; The ascii-rendering
+
+(defun asciipaint (w h)
+	(setf *pxs* 0)
+	(loop for y from 0 to (1- h) do
+		(loop for x from 0 to (1- w) do
+			(multiple-value-bind (z i) (m4nde1-1t3r (coords->c x y w h))
+				(if (< i *iterations*) (incf *pxs*)) ;; just for exiting
+				(princ 	(z->ascii2 i))))
+		(princ #\Newline))
+	(home))
+
+
+
+
+
 
 	
 
@@ -165,6 +157,8 @@
 	(format t "~c[H" (code-char 27)))
 
 
+;; _________________________________________________________________
+;;
 ;; Optimzed c0de
 ;; complex numbers are just cons of double float, all declared
 
@@ -203,7 +197,7 @@
 
 ;;; INteractive Mode
 (defun read-cmd ()
-	(let ((c (read-char-no-hang))) ;; Unfortunately we have to press enter after any cmd, because of terminal buf. Maybe go to ncurses soon
+	(let ((c (read-char-no-hang))) ;; FIXME: Unfortunately we have to press enter after any cmd, because of terminal buf. Maybe go to ncurses soon
 		(when c
 			(cond
 				((equal #\a c) (move -0.1 0))
--- threading.lisp
+++ threading.lisp
@@ -1,29 +1,46 @@
 ;;Lets speed shit up
 
-(defmacro segment-iter (func i n &optional (n-threads 4))
+(defmacro segment-iter (func n &optional (n-threads 4))
 	;;build n-threads iter specs
 	(cons 'list
 				(loop for ti from 0 to (1- n-threads) collect
-							`(lambda () (loop for ,i from ,ti to (1- ,n) by ,n-threads collect (funcall ,func ,i))))))
+							`(lambda () (loop for i from ,ti to (1- ,n) by ,n-threads collect (funcall ,func i))))))
 
 ;(segment-iter #'(lambda (x) x) i 12)
 
 
-(defun render-line (w y h)
+(defun render-line (w y h &key (colorizer #'z->ascii2))
 	(loop for x from 0 to (1- w)
 				collect
 				(multiple-value-bind (z i) (m4nde1-1t3r (coords->c x y w h) *iterations*)
 					(if (< i *iterations*) (incf *pxs*)) ;; just for exiting
-					(if (< i *iterations*)
-							(mod (round (* (/ i *iterations*) 255)) 255)
-							0))))
+					(funcall colorizer i))))
+			
 
-(defun render-line-ascii (w y h)  ;;fixme: combine
-	(loop for x from 0 to (1- w)
-				collect
-				(multiple-value-bind (z i) (m4nde1-1t3r (coords->c x y w h) *iterations*)
-					(if (< i *iterations*) (incf *pxs*)) ;; just for exiting
-					(z->ascii2 i))))
+(defun i->greyscale (i &optional (iterations *iterations*))
+	(if (< i iterations)
+			(mod (round (* (/ i iterations) 255)) 255)
+			0))
+
+
+
+(defun asciipaint-mp (w h)
+	(setf *pxs* 0)
+	(let* ((linebuf (make-array h))
+				 (funs (segment-iter #'(lambda (y)
+																 (setf (aref linebuf y) (render-line w y h)))
+									 h 10))
+			
+				 (threads (mapcar #'sb-thread:make-thread
+													funs)))
+		;;join threads
+		(mapcar #'sb-thread:join-thread threads)
+		(loop for line across linebuf DO
+			(mapcar #'princ
+							line)
+			(princ #\Newline)))
+	(home))
+
 
 
 ;(segment-iter #'(lambda (y) (render-line 100 y 16)) i 16)
@@ -58,23 +75,3 @@
 ;; 				do (setf *viewport* (next-vp))
 ;; 					 (pngpaint-mp (format nil "/home/bonk/coden/mandelbrot/image~4,'0d.png" i) 768 768)
 ;; 					 (format t ".")))
-
-
-
-
-(defun asciipaint-mp (w h)
-	(setf *pxs* 0)
-	(let* ((linebuf (make-array h))
-				 (funs (segment-iter #'(lambda (y)
-																 (setf (aref linebuf y) (render-line-ascii w y h)))
-									 i h 10))
-			
-				 (threads (mapcar #'sb-thread:make-thread
-													funs)))
-		;;join threads
-		(mapcar #'sb-thread:join-thread threads)
-		(loop for line across linebuf DO
-			(mapcar #'princ
-							line)
-			(princ #\Newline)))
-	(home))
